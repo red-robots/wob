53e02e5e2afb479ed43a5d8f408db3bc84c2db1e
Fritz <fre.healy@gmail.com>
2016-10-20 16:18:41 -0400

    adding menu support for primary menu

--- index.php
+++ index.php
@@ -12,8 +12,15 @@
       <script type="text/javascript">
         var wobbegong = {
             api_root: "<?php echo esc_url(get_rest_url(null,"wp/v2/"));?>",
+            menu_api_root: "<?php echo esc_url(get_rest_url(null,"wp-api-menus/v2/"));?>",
             root: "<?php echo trailingslashit(esc_url(get_bloginfo("url")));?>",
             install: "<?php echo preg_replace("/http:\/\/.+?\//i","",get_bloginfo("url"));?>",
+            domain: "<?php preg_match("/(http:\/\/.+?\/)/i",get_bloginfo("url"),$matches);
+                if(count($matches)>0){
+                    echo $matches[1];
+                } else {
+                    echo "";
+                }?>"
         };
       </script>
       <script src = "<?php echo get_template_directory_uri()."/build/index.js"?>"></script>
